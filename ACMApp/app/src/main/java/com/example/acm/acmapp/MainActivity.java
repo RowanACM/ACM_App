871f33dbc2b0c3582ade343add0228757c7fb678
tylercarberry <tylercarberry@gmail.com>
2016-04-08 15:14:15 -0400

    Enable javascript in webview

--- ACMApp/app/src/main/java/com/example/acm/acmapp/MainActivity.java
+++ ACMApp/app/src/main/java/com/example/acm/acmapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.acm.acmapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
         webView.loadUrl(ACM_URL);
+
     }
 }
