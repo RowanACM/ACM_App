2ac8b74b19e31e45bfc8205a6063e37cfc8b5698
tylercarberry <tylercarberry@gmail.com>
2016-04-08 15:10:57 -0400

    Add webview

--- ACMApp/app/src/main/java/com/example/acm/acmapp/MainActivity.java
+++ ACMApp/app/src/main/java/com/example/acm/acmapp/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.acm.acmapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String ACM_URL = "http://elvis.rowan.edu/acm/v1/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(ACM_URL);
     }
 }
