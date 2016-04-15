22d412f239a6da4702f8286ff600d0d7741b78bc
Nick Schillaci <nickschillaci1@gmail.com>
2016-04-15 16:13:06 -0400

    quick cleanup

--- ACMApp/app/src/main/java/com/example/acm/acmapp/MainActivity.java
+++ ACMApp/app/src/main/java/com/example/acm/acmapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.acm.acmapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,7 +9,6 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
     private final static String ACM_URL = "http://elvis.rowan.edu/acm/v1/";
-    private final static String ACM_CALENDAR_IFRAME_URL = "https://www.google.com/calendar/embed?showTz=0&amp;src=students.rowan.edu_gj0umqr01gtm1ofgc576fu3ujs%40group.calendar.google.com&amp;ctz=America/New_York";
     private WebView webView;
 
     @Override
@@ -23,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
 
         webView.loadData("<iframe src=\"https://www.google.com/calendar/embed?showTz=0&amp;src=students.rowan.edu_gj0umqr01gtm1ofgc576fu3ujs%40group.calendar.google.com&amp;ctz=America/New_York\" style=\"border: 0\" width=\"" + "100%" + "\" height=\"" + "100%" + "\" frameborder=\"0\" scrolling=\"no\"> </iframe>", "text/html", "utf-8");
-        //webView.loadUrl(ACM_CALENDAR_IFRAME_URL); // initial page load
+        //webView.loadUrl(ACM_URL); // initial page load
 
         // client overrides loadurl after initial page load (in app, not browser)
         class MyWebViewClient extends WebViewClient {
